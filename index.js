5641f1a47060271a18d9d37331b9938ef2430a2b
Srija S <srijakumar7@gmail.com>
2018-02-12 00:43:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function(){
     return "No, I must disagree";
 }
+
+BoardMember.prototype.doCharity = function(){
+   return "I like to help people."
+   }
+
+BoardMember.prototype.releasePressStatement = function(){
+return "You will see great things from Scuber."
+}
+
+BoardMember.prototypesayHi = function(){
+   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'   }
